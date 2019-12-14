ee43a9d44bdcd108c3cda93cdfcb18cd9e8cb674
jinju2828 <jinju2828@gmail.com>
2019-12-14 00:52:24 -0800

    Frontend - pulling data and show it

---
+++ frontend/components/Data.js
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import { ScrapeContext} from "./ScrapeContext";
+
+export default function Data() {
+    const scrapeData = useContext(ScrapeContext);
+    console.log(scrapeData);
+    return (
+        <div>
+            <h2>Scraping Data:</h2>
+            {scrapeData.hey}
+        </div>
+    );
+
+}
---
+++ frontend/components/Page.js
@@ -0,0 +1,14 @@
+import { ScrapeProvider } from "./ScrapeContext";
+
+export default function Page({children}){
+    return (
+        <ScrapeProvider value={{
+            hey: 'ho',
+            lets: 'go',
+        }}>
+            <div className="page">
+            {children}
+            </div>
+        </ScrapeProvider>
+    );
+}
